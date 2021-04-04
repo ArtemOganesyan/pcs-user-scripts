16483ca059ccb8174c745b44bce77103e86fd999
Artem Oganesyan <artem.oganesyan@gmail.com>
2021-04-04 16:50:49 +0300

    Correction with deployment

---
+++ NetCoreRunSample.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+yum -y update
+rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+yum -y install aspnetcore-runtime-5.0
+yum -y install dotnet-sdk-5.0
+yum -y install httpd
+yum -y install git
+echo "<VirtualHost *:80>
+   ServerName www.DOMAIN.COM
+   ProxyPreserveHost On
+   ProxyPass / http://127.0.0.1:5000/
+   ProxyPassReverse / http://127.0.0.1:5000/
+   RewriteEngine on
+   RewriteCond %{HTTP:UPGRADE} ^WebSocket$ [NC]
+   RewriteCond %{HTTP:CONNECTION} Upgrade$ [NC]
+   RewriteRule /(.*) ws://127.0.0.1:5000/$1 [P]
+   ErrorLog /var/log/httpd/netcore-error.log
+   CustomLog /var/log/httpd/netcore-access.log common
+</VirtualHost>" > /etc/httpd/conf.modules.d/20-netcore.conf
+service httpd start
+mkdir /var/netcore
+mkdir /var/netcore/source
+cd /var/netcore/source
+git clone https://github.com/ArtemOganesyan/pcs-webapp-main.git
+cd /var/netcore/source/pcs-webapp-main/MainWebApp
+dotnet run &
+
+# mkdir /var/netcore/deployment
+# dotnet publish -c Release -o /var/netcore/deployment
+# /var/netcore/deployment/MainWebApp
